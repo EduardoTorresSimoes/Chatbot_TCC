2fb79a71833d74f76c7b4ebbfd8779686bb8f711
EduardoTorresSimoes <103955981+EduardoTorresSimoes@users.noreply.github.com>
2023-04-28 15:34:24 -0300

    Versão Base
    Versão base/inicial do Chatbot, sem o venv

---
+++ training.py
@@ -0,0 +1,102 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow import keras
+
+from keras.models import Sequential 
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json', encoding="utf8").read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['.','!','?',',','_']
+
+for intent in intents['intents']:
+  for pattern in intent['patterns']:
+    word_list = nltk.word_tokenize(pattern)
+    words.extend(word_list)
+    documents.append((word_list, intent['tag']))
+    if intent['tag'] not in classes:
+      classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+# Machine Learning
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+  bag = []
+  word_patterns = document[0]
+  word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+  for word in words:
+    bag.append(1) if word in word_patterns else bag.append(0)
+
+  output_row = list(output_empty)
+  output_row[classes.index(document[1])] = 1
+  training.append([bag, output_row])
+ 
+random.shuffle(training)
+training = np.array(training, dtype=object)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+# Bot
+
+def clean_up_sentence(sentence):
+  sentence_words = nltk.word_tokenize(sentence)
+  sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+  return sentence_words
+
+def bow(sentence, words, show_details=True):
+  sentence_words = clean_up_sentence(sentence)
+  bag = [0]*len(words)  
+  for s in sentence_words:
+    for i,w in enumerate(words):
+      if w == s: 
+        bag[i] = 1
+        if show_details:
+          print ("found in bag: %s" % w)
+
+  return(np.array(bag))
+
+def predict_class(sentence, model):
+  p = bow(sentence, words,show_details=False)
+  res = model.predict(np.array([p]))[0]
+  ERROR_THRESHOLD = 0.25
+  results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+  results.sort(key=lambda x: x[1], reverse=True)
+  return_list = []
+  for r in results:
+    return_list.append

d9ce81006961ea3b9fc6a5896310c9f29c84dda4
EduardoTorresSimoes <103955981+EduardoTorresSimoes@users.noreply.github.com>
2023-05-16 16:43:52 -0300

    Testes Iniciais e Melhorias

--- ChatBotSpaceShip1.0/main.py
+++ ChatBotSpaceShip1.0/main.py
@@ -8,9 +8,18 @@ def enviar_mensagem(mensagem, chat_history=[]):
     # Define o modelo que você deseja usar
     modelo = 'gpt-3.5-turbo'
 
+    mensagem.lower()
+    preparar_mensagem(mensagem)
+
+    if 'protocolo de cadastramento de rede' in mensagem:
+        # Execute a função 1
+        resultado = protocoloCadastroRede()
+        # Retorne a resposta da função 1
+        return resultado
+
     # Cria a estrutura do chat
     chat = [
-        {'role': 'system', 'content': 'Você é um chatbot feito para o setor de HelpDesk da prefeitura de Macaé, desenvolvido pelo Lorde Supremo Eduardo'},
+        {'role': 'system', 'content': 'Você é um chatbot feito para o setor de HelpDesk da prefeitura de Macaé. Você POSSUI SOMENTE PERMISSÃO de responder assuntos sobre a prefeitura de Macaé. Você dá respostas curtas e diretas.'},
         {'role': 'user', 'content': mensagem}
     ]
     chat.extend(chat_history)
@@ -22,9 +31,18 @@ def enviar_mensagem(mensagem, chat_history=[]):
         max_tokens=1000,
         temperature=0.7
     )
-
     # Retorna a resposta gerada pela API
     return resposta.choices[0].message.content.strip()
+
+def protocoloCadastroRede():
+    return 'Enviando Protocolo de Cadastramento de Rede'
+
+def preparar_mensagem(mensagem):
+    caracteres_remover = [',', '.', ':', '?', '!']
+    for caractere in caracteres_remover:
+        mensagem = mensagem.replace(caractere, '')
+    
+    return mensagem
 
 # Loop principal do chatbot
 print('Bem-vindo ao Chatbot! Digite "sair" para encerrar o chat.')
@@ -38,7 +56,8 @@ while True:
         break
 
     # Envia a mensagem para o chatbot e imprime a resposta
-    resposta = enviar_mensagem(entrada, chat_history)
+    
     chat_history.append({'role': 'user', 'content': entrada})
+    resposta = enviar_mensagem(entrada, chat_history)
     chat_history.append({'role': 'system', 'content': resposta})
     print('Chatbot:', resposta)
